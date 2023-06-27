9ef848089bb0213644ceb19b80ce427be12db87f
FabianWild <Fabian.Wild1996@gmx.de>
2023-06-27 18:29:51 +0200

    forecast

--- main.js
+++ main.js
@@ -66,3 +66,51 @@ let controlElevation = L.control.elevation({
     theme: "zustieg"
 }).addTo(map);
 controlElevation.load("./data/sattelberg.gpx")
+
+// Wettervorhersage MET Norway
+async function showForecast(url, latlng) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    //console.log(jsondata, latlng);
+
+    let current = jsondata.properties.timeseries[0].data.instant.details;
+    //console.log(current);
+    let timestamp =new Date(jsondata.properties.meta.updated_at).toLocaleString();
+    let timeseries = jsondata.properties.timeseries;
+
+    let markup = `
+        <h4>Wetter für ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)} (${timestamp})</h4>
+        <table>
+            <tr><td>Luftdruck (hPa)</td><td>${current.air_pressure_at_sea_level}</td></tr>
+            <tr><td>Lufttemperatur (°C)</td><td>${current.air_temperature}</td></tr>
+            <tr><td>Bewölkungsgrad (%)</td><td>${current.cloud_area_fraction}</td></tr>
+            <tr><td>Relative Luftfeuchtigkeit (%)</td><td>${current.relative_humidity}</td></tr>
+            <tr><td>Windrichtung (°)</td><td>${current.wind_from_direction}</td></tr>
+            <tr><td>Windgeschwindigkeit (m/s)</td><td>${current.wind_speed}</td></tr>
+        </table>
+    `;
+
+    // Wettersymbole hinzufügen
+    for (let i=0; i<=24; i+=3) {
+        //console.log(timeseries[i]);
+        let icon = timeseries[i].data.next_1_hours.summary.symbol_code;
+        let image = `icons/${icon}.svg`;
+        markup += `<img src="${image}" style="width:32px;" title="${timeseries[i].time.toLocaleString()}">`
+        //console.log(icon, image);
+    }
+
+    L.popup().setLatLng(latlng).setContent(markup).openOn(themaLayer.forecast);
+
+}
+
+// auf Kartenklick reagieren
+map.on("click", function(evt) {
+    //console.log(evt.latlng.lat);
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`;
+    showForecast(url, evt.latlng);
+});
+
+// Klick auf Innsbruck simulieren
+map.fireEvent("click", {
+    latlng: L.latLng(ibk.lat, ibk.lng)
+})
